d6944172c794d1a0f046f765b91df5f084bb3b6d
kajalrpatel <kajalrpatel.ca@gmail.com>
2019-05-03 16:56:24 -0400

    2 files modified, 3 added

---
+++ src/app/aunction/aunction.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-aunction',
+  templateUrl: './aunction.component.html',
+  styleUrls: ['./aunction.component.css']
+})
+export class AunctionComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
